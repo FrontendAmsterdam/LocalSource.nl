b31c06baa364e1843865ca39f725aff1fa91b7b3
Robert Haritonov <r@rhr.me>
2016-08-15 09:17:32 +0200

    Update authors.js

--- authors.js
+++ authors.js
@@ -3,5 +3,5 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'operatino',        start: '15 Aug 2016', first: '762639967130644480', post: false }
+  { username: 'operatino',        start: '15 Aug 2016', first: '765082320143544320', post: false }
 ]);
