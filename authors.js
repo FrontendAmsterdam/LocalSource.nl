a5cb56622dd35dc30c9da80f988d28450e2c5aad
Robert Haritonov <r@rhr.me>
2016-08-22 14:08:08 +0200

    add @ramonvictor

--- authors.js
+++ authors.js
@@ -3,5 +3,6 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'operatino',        start: '15 Aug 2016', first: '765082320143544320', post: false }
+  { username: 'ramonvictor',        start: '22 Aug 2016', first: '767686602130849792', post: false },
+  { username: 'operatino',        start: '15 Aug 2016', first: '765082320143544320' }
 ]);
