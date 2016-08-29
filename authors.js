b4659d8d24db61c976cc3f8144758153d79d7176
Robert Haritonov <r@rhr.me>
2016-08-29 10:58:49 +0200

    add @norbertdelangen

--- authors.js
+++ authors.js
@@ -3,6 +3,7 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'ramonvictor',        start: '22 Aug 2016', first: '767686602130849792', post: false },
+  { username: 'norbertdelangen',        start: '29 Aug 2016', first: '770167208198729728', post: false },
+  { username: 'ramonvictor',        start: '22 Aug 2016', first: '767686602130849792'},
   { username: 'operatino',        start: '15 Aug 2016', first: '765082320143544320' }
 ]);
