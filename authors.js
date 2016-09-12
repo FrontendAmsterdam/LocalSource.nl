e4eca959a3fc7dd7855a2d53177a72fca638457e
Robert Haritonov <r@rhr.me>
2016-09-12 10:04:31 +0200

    add @kureevalexey

--- authors.js
+++ authors.js
@@ -3,7 +3,8 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'stefanomasini',        start: '5 Sep 2016', first: '772696400467558400', post: false },
+  { username: 'kureevalexey',        start: '12 Sep 2016', first: '775231940219826176', post: false },
+  { username: 'stefanomasini',        start: '5 Sep 2016', first: '772696400467558400'},
   { username: 'norbertdelangen',        start: '29 Aug 2016', first: '770167208198729728' },
   { username: 'ramonvictor',        start: '22 Aug 2016', first: '767686602130849792'},
   { username: 'operatino',        start: '15 Aug 2016', first: '765082320143544320' }
