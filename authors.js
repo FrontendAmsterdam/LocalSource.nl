c69ec21438484e53405e7c8a764d1c1990e7ae3d
Robert Haritonov <r@rhr.me>
2016-09-19 14:12:52 +0200

    add @watilde

--- authors.js
+++ authors.js
@@ -3,9 +3,10 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'kureevalexey',        start: '12 Sep 2016', first: '775231940219826176', post: false },
+  { username: 'watilde',        start: '19 Sep 2016', first: '777796712987500544', post: false },
+  { username: 'kureevalexey',        start: '12 Sep 2016', first: '775231940219826176'},
   { username: 'stefanomasini',        start: '5 Sep 2016', first: '772696400467558400'},
-  { username: 'norbertdelangen',        start: '29 Aug 2016', first: '770167208198729728' },
+  { username: 'norbertdelangen',        start: '29 Aug 2016', first: '770167208198729728'},
   { username: 'ramonvictor',        start: '22 Aug 2016', first: '767686602130849792'},
   { username: 'operatino',        start: '15 Aug 2016', first: '765082320143544320' }
 ]);
