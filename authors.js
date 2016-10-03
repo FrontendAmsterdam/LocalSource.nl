e2661283339577a25a2587eaf4c2b8e370d73868
Robert Haritonov <r@rhr.me>
2016-10-03 09:33:17 +0200

    add @_paulverbeek

--- authors.js
+++ authors.js
@@ -3,7 +3,8 @@ import authorId from './helpers/author-id';
 
 export default authorId([
   // post: false
-  { username: 'watilde',        start: '19 Sep 2016', first: '777796712987500544', post: false },
+  { username: '_paulverbeek',        start: '3 Oct 2016', first: '782839395741208576', post: false },
+  { username: 'watilde',        start: '19 Sep 2016', first: '777796712987500544'},
   { username: 'kureevalexey',        start: '12 Sep 2016', first: '775231940219826176'},
   { username: 'stefanomasini',        start: '5 Sep 2016', first: '772696400467558400'},
   { username: 'norbertdelangen',        start: '29 Aug 2016', first: '770167208198729728'},
